564411e89875f053d291beffe1dbb2c4f0d277fe
Juan Carlos Aristizabal V <juan.aristizabal@Juans-MBP.une.net.co>
2017-11-20 22:00:14 -0500

    My own model

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,13 @@ FROM python:3.5.3
 
 WORKDIR /app/
 
+
 COPY requirements.txt /app/
 RUN pip install -r ./requirements.txt
 
 COPY app.py __init__.py /app/
 
+COPY model.pkl /app/
 # ENTRYPOINT /bin/bash
 EXPOSE 5000
 
--- app.py
+++ app.py
@@ -1,30 +1,43 @@
 #!flask/bin/python
 from flask import Flask
+from flask import session
 from flask import request
+import uuid
 import pandas as pd
 from sklearn import linear_model
 import pickle
 
-# creating and saving some model
-reg_model = linear_model.LinearRegression()
-reg_model.fit([[1.,1.,5.], [2.,2.,5.], [3.,3.,1.]], [0.,0.,1.])
-pickle.dump(reg_model, open('some_model.pkl', 'wb'))
+
+
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'q3RD7Urbz72xGhsjJKv'
+
+
 
 @app.route('/isAlive')
 def index():
     return "true"
 
+
+
 @app.route('/prediction/api/v1.0/some_prediction', methods=['GET'])
 def get_prediction():
-    feature1 = float(request.args.get('f1'))
-    feature2 = float(request.args.get('f2'))
-    feature3 = float(request.args.get('f3'))
-    loaded_model = pickle.load(open('some_model.pkl', 'rb'))
-    prediction = loaded_model.predict([[feature1, feature2, feature3]])
+    feature1 = float(request.args.get('day'))
+    feature2 = float(request.args.get('hour'))
+    loaded_model = pickle.load(open('model.pkl', 'rb'))
+    prediction = loaded_model.predict([[feature1, feature2]])
     return str(prediction)
-   
+
+
+
+@app.route('/image')
+def image():
+    return "QQBQ"
+
+
+
 if __name__ == '__main__':
     app.run(port=5000,host='0.0.0.0')        
     # app.run(debug=True)
